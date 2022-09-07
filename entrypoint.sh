452ad19dadeef98a864aa436c2f4d2a02bb57cf1
Sergio Talens-Oliag <sto@kyso.io>
2022-09-07 23:26:43 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -48,7 +48,6 @@ print_hardlink_yml() {
 EOF
 }
 
-
 print_s3import_yml() {
   cat <<EOF
 - id: s3import
@@ -103,7 +102,6 @@ print_s3import_yml() {
 EOF
 }
 
-
 print_token_yml() {
   if [ "$1" ]; then
     cat << EOF
@@ -118,40 +116,46 @@ EOF
   fi
 }
 
+exec_webhook() {
+  # Validate WORKDIR
+  if [ -z "$WEBHOOK_WORKDIR" ]; then
+    echo "Must define the WEBHOOK_WORKDIR variable!" >&2
+    exit 1
+  fi
+  WORKDIR="$(realpath "$WEBHOOK_WORKDIR" 2>/dev/null)" || true
+  if [ ! -d "$WORKDIR" ]; then
+    echo "The WEBHOOK_WORKDIR '$WEBHOOK_WORKDIR' is not a directory!" >&2
+    exit 1
+  fi
+  # Get TOKENS, if the DU_TOKEN or HARDLINK_TOKEN is defined that is used, if not
+  # if the COMMON_TOKEN that is used and in other case no token is checked (that
+  # is the default)
+  DU_TOKEN="${DU_TOKEN:-$COMMON_TOKEN}"
+  HARDLINK_TOKEN="${HARDLINK_TOKEN:-$COMMON_TOKEN}"
+  S3_TOKEN="${S3_TOKEN:-$COMMON_TOKEN}"
+  # Create webhook configuration
+  {
+    print_du_yml
+    print_token_yml "$DU_TOKEN"
+    echo ""
+    print_hardlink_yml
+    print_token_yml "$HARDLINK_TOKEN"
+    echo ""
+    print_s3import_yml
+    print_token_yml "$S3_TOKEN"
+  }>"$WEBHOOK_YML"
+  # Run the webhook command
+  # shellcheck disable=SC2086
+  exec webhook -hooks "$WEBHOOK_YML" $WEBHOOK_OPTS
+}
+
 # ----
 # MAIN
 # ----
 
-# Validate WORKDIR
-if [ -z "$WEBHOOK_WORKDIR" ]; then
-  echo "Must define the WEBHOOK_WORKDIR variable!" >&2
-  exit 1
-fi
-WORKDIR="$(realpath "$WEBHOOK_WORKDIR" 2>/dev/null)" || true
-if [ ! -d "$WORKDIR" ]; then
-  echo "The WEBHOOK_WORKDIR '$WEBHOOK_WORKDIR' is not a directory!" >&2
-  exit 1
-fi
+case "$1" in
+"server") exec_webhook ;;
+*) exec "$@" ;;
+esac
 
-# Get TOKENS, if the DU_TOKEN or HARDLINK_TOKEN is defined that is used, if not
-# if the COMMON_TOKEN that is used and in other case no token is checked (that
-# is the default)
-DU_TOKEN="${DU_TOKEN:-$COMMON_TOKEN}"
-HARDLINK_TOKEN="${HARDLINK_TOKEN:-$COMMON_TOKEN}"
-S3_TOKEN="${S3_TOKEN:-$COMMON_TOKEN}"
-
-# Create webhook configuration
-{ 
-  print_du_yml
-  print_token_yml "$DU_TOKEN"
-  echo ""
-  print_hardlink_yml
-  print_token_yml "$HARDLINK_TOKEN"
-  echo ""
-  print_s3import_yml
-  print_token_yml "$S3_TOKEN"
-}>"$WEBHOOK_YML"
-
-# Run the webhook command
-# shellcheck disable=SC2086
-exec webhook -hooks "$WEBHOOK_YML" $WEBHOOK_OPTS
+# vim: ts=2:sw=2:et
